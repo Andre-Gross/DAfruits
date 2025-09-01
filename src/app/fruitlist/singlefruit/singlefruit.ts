b22cca4a629e6f04afbf0f7ff22b4fd486e9af7a
Andre-Gross <andre.handy94@gmail.com>
2025-09-01 19:25:39 +0200

    chore(fruitlist): add SingleFruit component with standard config (without spec.ts)

---
+++ src/app/fruitlist/singlefruit/singlefruit.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-singlefruit',
+  imports: [],
+  templateUrl: './singlefruit.html',
+  styleUrl: './singlefruit.scss'
+})
+export class Singlefruit {
+
+}
